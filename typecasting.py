251f45641c19651181f36e4571337d190c571123
Harika2519 <harikamavunuri@gmail.com>
2025-01-29 23:26:36 +0000

    typecasting prgm

---
+++ typecasting.py
@@ -0,0 +1,13 @@
+#the conversion of one data type to another data type 
+s=(input ("enter your number\n"))
+p=(input ("enter your number\n"))
+print(s+p)
+l=print (int(s)+ int (p))
+
+#if you mention something in quots '' or "" then it is called as string 
+
+a=2
+b=2.5
+print (type (a))
+print (type (b))
+print (type (a+b))
